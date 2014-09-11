385444dcc81815e8fe2fd0e3f0c599ebabe7b77b
Philip Basford <pjb@ecs.soton.ac.uk>
2014-09-11 19:40:42 +0100

    Now only gets last 10 records

--- reader/dbprocess.sh
+++ reader/dbprocess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'SELECT device_id, timestamp, HEX(data) FROM feshie.unprocessed_data' |
-  mysql -udjap1g11 -papplebanana --skip-column-names |
+echo 'SELECT * FROM `unprocessed_data` WHERE id > ( (SELECT MAX(id) FROM unprocessed_data ) -10)' |
+  mysql -udjap1g11 -papplebanana --skip-column-names feshie |
   while read row
   do
     id=`echo $row | cut -f 1 -d" "`
