ce45edde1b70182ecf61267fb7cff5a4767abb10
Philip Basford <pjb@ecs.soton.ac.uk>
2014-12-08 17:09:56 +0000

    Makefile and includes tidied

--- reader/reader.c
+++ reader/reader.c
@@ -1,9 +1,7 @@
 #include "pb_decode.h"
 #include "pb_encode.h"
-#include "pb_encode.c"
-#include "pb_decode.c"
 #include "readings.pb.h"
-#include "readings.pb.c"
+#include "rs485_message.pb.h"
 #include <stdio.h>
 #include <math.h>
 
