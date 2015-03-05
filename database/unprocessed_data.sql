fcddaa0eb1a7599fd272fd0871f520f9e8cc6e96
Philip Basford <pjb@ecs.soton.ac.uk>
2015-03-05 15:22:21 +0000

    Now matches the php description.  Also includes test table

--- database/unprocessed_data.sql
+++ database/unprocessed_data.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 10, 2014 at 10:09 AM
+-- Generation Time: Mar 05, 2015 at 03:21 PM
 -- Server version: 5.1.73
--- PHP Version: 5.3.2-1ubuntu4.26
+-- PHP Version: 5.3.2-1ubuntu4.28
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
@@ -22,12 +22,35 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `test_unprocessed_data`
+--
+
+CREATE TABLE IF NOT EXISTS `test_unprocessed_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `device_id` char(4) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `data` blob NOT NULL,
+  `unpacked` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `device_id` (`device_id`),
+  KEY `timestamp` (`timestamp`),
+  KEY `unpacked` (`unpacked`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=40020 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `unprocessed_data`
 --
 
 CREATE TABLE IF NOT EXISTS `unprocessed_data` (
-  `ID` char(4) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `device_id` char(4) NOT NULL,
   `timestamp` datetime NOT NULL,
   `data` blob NOT NULL,
-  PRIMARY KEY (`ID`,`timestamp`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `unpacked` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `device_id` (`device_id`),
+  KEY `timestamp` (`timestamp`),
+  KEY `unpacked` (`unpacked`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28083 ;
